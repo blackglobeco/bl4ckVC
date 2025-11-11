125090e376d0ebfeabb182e1be574db40c9ee607
Black Globe <137195478+blackglobeco@users.noreply.github.com>
2025-11-11 12:14:34 +0800

    Add files via upload

---
+++ chatterbox/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.1.4"
+
+from .tts import ChatterboxTTS
+from .vc import ChatterboxVC
+from .mtl_tts import ChatterboxMultilingualTTS
+
+__all__ = ['ChatterboxTTS', 'ChatterboxVC', 'ChatterboxMultilingualTTS']
